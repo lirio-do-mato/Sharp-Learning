e6cb9ae7bf21cfbf347e66066084ac5367fb8b12
Marcelo Sicoli <49480702+Marcelo-Sicoli@users.noreply.github.com>
2021-06-02 11:16:37 -0300

    Consertei um negocio na tabela

--- Backend/SharpLearning_Tables.sql
+++ Backend/SharpLearning_Tables.sql
@@ -4,7 +4,7 @@ nome varchar(100),
 idade int not null,
 ondeMora varchar(100),
 ocupacao varchar(100),
-educacaoMusical varchar(100),
+educacaoMusical varchar(100) not null,
 tocaInstrumento varchar (100))
 
 select * from tcc_infoQuestionados
