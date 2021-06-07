71f798a56b5a74eee5e7219715d77b6a881c364e
lirio-do-mato <57483891+lirio-do-mato@users.noreply.github.com>
2021-06-07 15:46:38 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react'
-import Cabecalho from './components/Cabecalho'
-import Cores from './data/cores'
-import Logo from './components/Logo'
-import Blob from './components/Blob'
+import PrimeiraPagina from './components/PrimeiraPagina'
+import SegundaPagina from './components/SegundaPagina'
 import './App.css'
 
 
 export default props =>
     <div className='geral'>
-        <Cabecalho
-        titulo="Sharp Learning"
-        back={Cores.magenta}
-        fore={Cores.rosa}/>
-        <Logo/>
-        <Blob/>
+        <PrimeiraPagina/>
+        <SegundaPagina/>
     </div>
