2668d910978ad89e40d85e9f96c480c1ba39c434
lirio-do-mato <57483891+lirio-do-mato@users.noreply.github.com>
2021-06-09 09:22:03 -0300

    Add files via upload

---
+++ Perguntas/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+import PrimeiraPagina from './components/PrimeiraPagina'
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<PrimeiraPagina/>
+			</div>
+		)
+	}
+}
+
+export default App
