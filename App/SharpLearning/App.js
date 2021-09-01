f26e3c93a9583c8194c188c9ae317c9c83a90bc8
Marcelo Sicoli <49480702+Marcelo-Sicoli@users.noreply.github.com>
2021-09-01 11:31:08 -0300

    Cadastro on

--- App/SharpLearning/App.js
+++ App/SharpLearning/App.js
@@ -6,142 +6,170 @@
  * @flow strict-local
  */
 
- import React, { useState } from 'react';
- import type {Node} from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   Button,
-   StyleSheet,
-   Text,
-   TextInput,
-   useColorScheme,
-   View,
-   Picker,
- } from 'react-native';
- 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
- const App: () => Node = () => {
-   const isDarkMode = useColorScheme() === 'dark';
- 
-   const [nome, setNome] = useState('');
-   const [idade, setIdade] = useState('');
-   const [email, setEmail] = useState('');
-   const [senha, setSenha] = useState('');
-   const [senha2, setSenha2] = useState('');
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
-   function send() {
-     if(nome == '')
-       alert("Preencha o seu nome");
-     else
-     {
-       if(idade == '')
-         alert("Preencha a sua idade");
-       else
-       {
-         if(email == '')
-           alert("Preencha o seu email");
-         else
-         {
-           if(senha == '')
-             alert("Preencha a sua senha");
-           else
-           {
-             if(senha2 == '')
-               alert("Preencha a confirmação da sua senha");
-             else
-             {
-               if(senha != senha2)
-                 alert("As senhas devem ser iguais");
-               else
-               {
-                 fetch('http://143.106.201.0:3001/addUsers', {
-                   method: 'post',
-                   headers: {
-                     'Accept':'application/json','Content-Type': 'application/json'
-                   },
-                   body: JSON.stringify({
-                     nome: nome,
-                     idade: idade,
-                     email: email,
-                     educacao: 'Mediano',
-                     toca: 'Violão',
-                     senha: senha
-                   })
-                 }).then(res => res.text())
-                 .then(data => 
+import React, { useState } from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+  Picker,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => Node = () => {
+
+
+const [nome, setNome] = useState('');
+const [idade, setIdade] = useState('');
+const [email, setEmail] = useState('');
+const [educacao, setEducacao] = useState('');
+const [instrumento, setInst] = useState('');
+const [senha, setSenha] = useState('');
+const [senha2, setSenha2] = useState('');
+
+function send() {
+  if(nome == '')
+    alert("Preencha o seu nome");
+  else
+  {
+    if(idade == '')
+      alert("Preencha a sua idade");
+    else
+    {
+      if(email == '')
+        alert("Preencha o seu email");
+      else{
+        if(instrumento=='')
+          alert("Escolha um instrumento para acompanhar este curso, as opções validas atualmente são: Violão, Canto e Violino");
+        else{
+          if(instrumento.toLowerCase()!='canto'&&instrumento.toLowerCase()!='violão'&&instrumento.toLowerCase()!='violino'&&instrumento.toLowerCase()!='nenhum')
+            alert("Escolha um instrumento valido, as opções atuais são: Nenhum, Violão, Canto e Violino");
+          else{
+            if(educacao=='')
+              alert("Defina seu nivel de educação musical. As opções validas são: Nenhuma, Basica, Intermediaria, Avançada");
+            else{
+              if(educacao.toLowerCase()!='nenhuma'&&educacao.toLowerCase()!='basica'&&educacao.toLowerCase()!='intermediaria'&&educacao.toLowerCase()!='avançada')
+                alert("Defina seu nivel de educação musical. As opções validas são: Nenhuma, Basica, Intermediaria, Avançada");
+              else
+              {
+                if(senha == '')
+                  alert("Preencha a sua senha");
+                else
+                {
+                  if(senha2 == '')
+                    alert("Preencha a confirmação da sua senha");
+                  else
                   {
-                    alert("Usuario cadastrado com sucesso");
-                  })
-                  .catch(erro => {
-                    alert(erro)
-                  })
-               }
-             }
-           }
-         }
-       }
-     }
-   }
+                    if(senha != senha2)
+                      alert("As senhas devem ser iguais");
+                    else
+                    {
+                      fetch('http://143.106.203.210:3001/addUsers', {
+                        method: 'post',
+                        headers: {
+                          'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                          nome: nome,
+                          idade: idade,
+                          email: email,
+                          educacao: educacao,
+                          toca: instrumento,
+                          senha: senha
+                        })
+                      })
+                      .then(res => res.text())
+                      .then(data => {alert("Usuario cadastrado com sucesso");})
+                      .catch(erro => {alert(erro)})
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }  
+    }
+  }
+}
  
-   return (
-     <View >
-     <Text style={[styles.sectionTitle,{color: isDarkMode ? Colors.white : Colors.black,},]}> Cadastro </Text>
-     <View>
-       <TextInput placeholder="Nome" onChangeText={nome => setNome(nome)} defaultValue={nome}/>
-       <TextInput placeholder="Idade" onChangeText={idade => setIdade(idade)} defaultValue={idade}/>
-       <TextInput placeholder="Email" onChangeText={email => setEmail(email)} defaultValue={email}/>
-       <Text style={[{color: isDarkMode ? Colors.white : Colors.black,},]}>Escolha o instrumento que acompanhara este app</Text>
-       <Picker style={[{color: isDarkMode ? Colors.white : Colors.black,},]}>
-         <Picker.Item label="Nenhum" value="nenhum" />
-         <Picker.Item label="Baixo" value="baixo" />
-         <Picker.Item label="Bateria" value="Bateria" />
-         <Picker.Item label="Canto" value="canto" />
-         <Picker.Item label="Piano" value="piano" />
-         <Picker.Item label="Violão" value="violão" />
-         <Picker.Item label="Violino" value="violino" />
-       </Picker>
-       <Text style={[{color: isDarkMode ? Colors.white : Colors.black,},]}>Selecione qual descreve melhor sua educação musical</Text>
-       <Picker style={[{color: isDarkMode ? Colors.white : Colors.black,},]}>
-         <Picker.Item label="Nenhum" value="nenhum" />
-         <Picker.Item label="Basico" value="basico" />
-         <Picker.Item label="Mediano" value="medio" />
-         <Picker.Item label="Avançado" value="avançado" />
-       </Picker>
-       <TextInput name="senha" secureTextEntry={true} placeholder="Senha" onChangeText={senha => setSenha(senha)} defaultValue={senha}/>
-       <TextInput name="confirmeSenha" secureTextEntry={true} placeholder="Confirme a Senha" onChangeText={senha2 => setSenha2(senha2)} defaultValue={senha2}/>
-       <Button onPress={send} title="Enviar" color="#841584"/>
-     </View>
-   </View>
-   );
- };
+  return (
+    <View style={styles.background}>
+      <Text style={styles.sectionTitle}> Cadastro </Text>
+      <View>
+        <TextInput style={styles.sectionDescription} placeholder="Nome" onChangeText={nome => setNome(nome)} defaultValue={nome}/>
+        <TextInput style={styles.sectionDescription} placeholder="Idade" onChangeText={idade => setIdade(idade)} defaultValue={idade}/>
+        <TextInput style={styles.sectionDescription} placeholder="Email" onChangeText={email => setEmail(email)} defaultValue={email}/>
+        <TextInput style={styles.sectionDescription} placeholder="Educação Musical" onChangeText={educacao => setEducacao(educacao)} defaultValue={educacao}/>
+        <TextInput style={styles.sectionDescription} placeholder="Toca instrumento" onChangeText={instrumento => setInst(instrumento)} defaultValue={instrumento}/>
+        <TextInput style={styles.sectionDescription} name="senha" secureTextEntry={true} placeholder="Senha" onChangeText={senha => setSenha(senha)} defaultValue={senha}/>
+        <TextInput style={styles.sectionDescription} name="confirmeSenha" secureTextEntry={true} placeholder="Confirme a Senha" onChangeText={senha2 => setSenha2(senha2)} defaultValue={senha2}/>
+        <View style={styles.sectionAlign}>
+          <TouchableOpacity onPress={send} style={styles.roundButton1}>
+            <Text style={styles.sectionButtonText}>Enviar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 20,
+  },
+  background: {
+      backgroundColor: '#46424A',
+  },
+  sectionTitle: {
+    color:'#F7F4EB',
+    textAlign:'center',
+    fontSize: 26,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    paddingTop: 8,
+    color: '#F7F4EB',
+    paddingHorizontal: 10,
+    fontSize: 16,
+    fontWeight: '400',
+  },
+  sectionAlign:{
+    paddingTop:20,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',  
+    backgroundColor: '#46424A',
+  },
+  roundButton1: {
+    width: 125,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize:16,
+    borderRadius: 20,
+    backgroundColor: '#420B2F',
+  },
+  sectionButtonText:{
+    color: '#F7F4EB',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
  
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
- 
- export default App;
+export default App;
