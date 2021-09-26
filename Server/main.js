b0ad77a01e412444b55d21bb334e440a8e7c57fe
lirio-do-mato <57483891+lirio-do-mato@users.noreply.github.com>
2021-09-26 16:57:39 -0300

    Add files via upload

--- Server/main.js
+++ Server/main.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3001;
 require('dotenv').config();
 const sql = require("mssql");
-const {urlencoded, json} = require("body-parser");
-const strConn = 
-    `Server=regulus.cotuca.unicamp.br;`+ //process.env.DB_SERVER
-    `Database=BD19185;`+ //process.env.DB_BASE
-    `User Id=BD19185;`+ //process.env.DB_USER
+const { urlencoded, json } = require("body-parser");
+const strConn =
+    `Server=regulus.cotuca.unicamp.br;` + //process.env.DB_SERVER
+    `Database=BD19185;` + //process.env.DB_BASE
+    `User Id=BD19185;` + //process.env.DB_USER
     `Password=Cotuca PD 19`; //process.env.DB_PASSWORD
 
 //console.log(strConn);
@@ -15,15 +15,15 @@ sql.connect(strConn)
     .then(conn => (global.conn = conn))
     .catch(e => console.log(e));
 
-app.use(urlencoded({extended: true}));
+app.use(urlencoded({ extended: true }));
 app.use(json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        {Accept: 'application/json',
-        'Content-Type': 'application/json'}
-    );
+    res.header({
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    });
     res.header(
         "Access-Control-Allow-Methods",
         "GET, POST, HEAD, OPTIONS, PATCH, DELETE"
@@ -32,8 +32,8 @@ app.use(function(req, res, next) {
 });
 
 const router = express.Router();
-router.get("/", (req, res) => 
-    res.json({ message: "API running on port 3001"})
+router.get("/", (req, res) =>
+    res.json({ message: "API running on port 3001" })
 );
 app.use("/", router);
 
@@ -42,23 +42,23 @@ console.log("teste");
 
 function executeQuery(sql, res) {
     global.conn
-      .request()
-      .query(sql)
-      .then(result => {
-        let obj = result.recordset[0];
-        let ret = obj[""];
-        console.log(ret);
-        if(ret == 1) {
-            res.status(200);
-            res.json(result.recordset[0]);
-        } else 
-        {
-            res.status(404);
-        }})
-      .catch(e => res.json(e));
+        .request()
+        .query(sql)
+        .then(result => {
+            let obj = result.recordset[0];
+            let ret = obj[""];
+            console.log(ret);
+            if (ret == 1) {
+                res.status(200);
+                res.json(result.recordset[0]);
+            } else {
+                res.status(404);
+            }
+        })
+        .catch(e => res.json(e));
 };
 
-router.get("/teste", (req, res) => 
+router.get("/teste", (req, res) =>
     executeQuery(`select * from Paciente`, res)
 );
 
